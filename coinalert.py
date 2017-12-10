b2612bcde75bfacfeb9a5ad65913ace41a6cc9f5
spurnout <spurnout@gmail.com>
2017-12-09 18:12:25 -0800

    Update coinalert.py
    Updated with timers and additional SMS message added if I wanted to let more than one person know, this is a temporary workaround until I can build an array of numbers to be included and just call each one in turn. from twilio docs "The best way to do this is to build an array of the recipients and iterate through each phone number."

--- coinalert.py
+++ coinalert.py
@@ -1,14 +1,17 @@
 import json
 from bittrex import Bittrex
 from twilio.rest import Client
+import time, threading
 
-account = "xxxxxxxxxx"
-token = "xxxxxxxxxxxx"
+# twilio account info
+account = "xxxxxxxxxxx"
+token = "xxxxxxxxxxxxxxxxxx"
 client = Client(account, token)
 
-api = Bittrex("xxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxx")
+#bittrex api key and secret
+api = Bittrex("xxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxx")
 
-"""
+""" # more example for me than anything
 print api.get_balance('XLM')
 {'success': True, 
  'message': '',
@@ -17,18 +20,42 @@ print api.get_balance('XLM')
 }
 """
 
-print api.get_marketsummary('BTC-XLM')
-response = api.get_marketsummary('BTC-XLM')
-last = response['result'][0]['Last']
-print last
+def get_coin_value():
+    while True:
+        print api.get_marketsummary('BTC-XLM')
+        response = api.get_marketsummary('BTC-XLM')
+        #making last global below is probably useless since i moved the test_and_text into this function, might still want to separate in the future and keep last as a global variable though
+        global last
+        last = response['result'][0]['Last']
+        print last
+        # test to see if the price of BTC-XLM has risen above 0.00000975
+        if last >= 9.75e-06:
+            print "great!"
+            message = client.messages.create(to="+18181234567", from_="+18181234567",
+                                    body="Price equal to or higher than 0.00000975")
+            message = client.messages.create(to="+18181234567", from_="+18181234567",
+                                    body="Price equal to or higher than 0.00000975")
+            time.sleep(270)
+        else:
+            print "bah!"
+            #message = client.messages.create(to="+18186531589", from_="+18189753284",
+            #                            body="Price equal to or lower than 0.00001000")
+        time.sleep(30)
 
 # test to see if the price of BTC-XLM has risen above 0.000010000
 
-if last >= 1.00e-05:
-    print "great!"
-    message = client.messages.create(to="+11234567890", from_="+11234567890",
-                                 body="Price equal to or higher than 0.00001000")
-else:
-    print "bah!"
-    message = client.messages.create(to="+11234567890", from_="+11234567890",
-                                 body="Price equal to or lower than 0.00001000")
+def test_and_text():
+    if last >= 9.75e-06:
+        print "great!"
+        message = client.messages.create(to="+18181234567", from_="+18181234567",
+                                    body="Price equal to or higher than 0.00000975")
+        message = client.messages.create(to="+18181234567", from_="+18181234567",
+                                    body="Price equal to or higher than 0.00000975")
+    else:
+        print "bah!"
+        #message = client.messages.create(to="+18186531589", from_="+18189753284",
+        #                            body="Price equal to or lower than 0.00001000")
+                                    
+get_coin_value()
+test_and_text()
+
